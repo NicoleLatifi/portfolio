2e883ae29bac06be65344ac739e8ffc7b37147c6
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-03 21:24:55 -0700

    Add id and use as argument

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,14 +1,15 @@
 interface Props {
+  id: string,
   variant: string,
   name: string,
-  onClick: () => void;
+  onClick: (productId: string) => void;
 }
 
 function Button(props: Props) {
   return (
     <a 
       className={props.variant} 
-      onClick={props.onClick}
+      onClick={() => props.onClick(props.id)}
     >
       {props.name}
     </a>
