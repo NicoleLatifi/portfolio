39a6416293d83af091ae9898e1f59755aa0bbe97
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-03 20:41:18 -0700

    Create addToCart function and pass as props

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css';
 import ProductsList from './Components/ProductsList/ProductsList'
 
 function App(): JSX.Element {
+  const addToCart = () => {
+    console.log("Product added to cart!")
+  };
+
   return (
     <div>
       <h1>Nicole Latifi</h1>
-      <ProductsList />
+      <ProductsList addToCart={addToCart} />
     </div>
-  );
+  )
 }
 
 export default App;
