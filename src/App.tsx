a2c741cedf789074f823ec4305191f7fc98822d9
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-03 21:23:22 -0700

    Import products and add productId argument

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import ProductsList from './Components/ProductsList/ProductsList'
+import { products } from './products-data'
 
 function App(): JSX.Element {
-  const addToCart = () => {
-    console.log("Product added to cart!")
-  };
+  const addToCart = (productId: string) => {
+    console.log(`${products[productId].name} added to cart!`)
+  }
 
   return (
     <div>
