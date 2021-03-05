814a3a678c2cb68de5ff9ca01b8a8d1d3a7ce1d0
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-04 21:18:53 -0700

    Add Redux and call setProductsData in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import './App.css';
-import ProductsList from './Components/ProductsList/ProductsList'
+import './App.css'
+import { useEffect } from 'react'
+import ProductsList from './components/ProductsList/ProductsList'
 import { products } from './products-data'
+import {ProductsLibrary} from './products-data'
+import { connect } from 'react-redux'
+import { setProductsData } from './store/products/actions'
+import { bindActionCreators } from 'redux'
 
-function App(): JSX.Element {
+interface DispatchProps {
+  setProductsData: (products: ProductsLibrary) => void
+}
+
+function App(props: DispatchProps): JSX.Element {
+  useEffect(() => {
+    props.setProductsData(products)
+  }, [])
+
   const addToCart = (productId: string) => {
     console.log(`${products[productId].name} added to cart!`)
   }
@@ -15,4 +28,10 @@ function App(): JSX.Element {
   )
 }
 
-export default App;
+export const mapDispatchToProps = (dispatch: any) => (
+  bindActionCreators({
+    setProductsData
+  }, dispatch)
+)
+
+export default connect(null, mapDispatchToProps)(App);
