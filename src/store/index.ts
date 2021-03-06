269883b915a6c119a17d0b3905b197a01e7d6910
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-05 21:07:37 -0700

    Add cartReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { productsReducer } from './products/reducers'
+import { cartReducer } from './cart/reducers'
 
 export const rootReducer = combineReducers({
   products: productsReducer,
+  cart: cartReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
