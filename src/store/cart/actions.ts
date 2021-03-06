213b48a48fe94a7ac7ddae54f62658534a0dbf33
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-05 22:03:06 -0700

    Create addIdToCart function

--- src/store/cart/actions.ts
+++ src/store/cart/actions.ts
@@ -1,7 +1,14 @@
-import { SET_CART } from './types'
+import { CartActionTypes, SET_CART, ADD_ID_TO_CART } from './types'
 
-export function setCart() {
+export function setCart(): CartActionTypes {
   return {
     type: SET_CART
+  }
+}
+
+export function addIdToCart(id: string): CartActionTypes {
+  return {
+    type: ADD_ID_TO_CART,
+    payload: id
   }
 }
