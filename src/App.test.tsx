91b386cad3741b202a4720da76a3eef85b61823f
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-08 21:03:00 -0700

    Test that header and product name are rendered

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const mockStore = configureStore([])
+const store = mockStore({
+  "1": {
+    id: "1",
+    name: "Resume"
+  }
+})
+
+test('Renders header', () => {
+  render(
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  )
+  const header = screen.getByText('Nicole Latifi')
+  expect(header).toBeInTheDocument()
+})
+
+test('Renders product name', () => {
+  render(
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  )
+  const name = screen.getByText('Resume')
+  expect(name).toBeInTheDocument()
+})
