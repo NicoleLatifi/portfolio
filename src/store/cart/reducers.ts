5992ab0532a8f8c14e2c670e257e929f2d2e8154
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-16 20:33:27 -0600

    Fix remove bug using delete operator

--- src/store/cart/reducers.ts
+++ src/store/cart/reducers.ts
@@ -1,4 +1,4 @@
-import { CartActionTypes, Cart, SET_CART, ADD_ID_TO_CART, REMOVE_ID_FROM_CART } from './types'
+import { CartActionTypes, CartType, SET_CART, ADD_ID_TO_CART, REMOVE_ID_FROM_CART } from './types'
 
 const initialState: any = {
   cart: {}
@@ -7,16 +7,18 @@ const initialState: any = {
 export function cartReducer(
   state = initialState.cart,
   action: CartActionTypes
-): Cart {
+): CartType {
+  let newCart
   switch (action.type) {
     case SET_CART:
       return state
     case ADD_ID_TO_CART:
-      let newCart = {...state}
+      newCart = {...state}
       newCart[action.payload.id] = {"quantity": action.payload.quantity}
       return newCart
     case REMOVE_ID_FROM_CART:
-      newCart = state.filter((id: string) => !action.payload.includes(id))
+      newCart = {...state}
+      delete newCart[action.payload]
       return newCart
     default: return state
   }
