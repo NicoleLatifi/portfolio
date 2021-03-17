5177ada2cf77f6c38123f9d113b392fd0dcfaaae
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-17 15:34:50 -0600

    Add DECREASE_QUANTITY and INCREASE_QUANTITY

--- src/store/cart/types.ts
+++ src/store/cart/types.ts
@@ -8,19 +8,36 @@ type CartItemType = {
 
 export const SET_CART = 'SET_CART'
 export const ADD_ID_TO_CART = 'ADD_ID_TO_CART'
-export const REMOVE_ID_FROM_CART= 'REMOVE_ID_FROM_CART'
+export const REMOVE_ID_FROM_CART = 'REMOVE_ID_FROM_CART'
+export const DECREASE_QUANTITY = 'DECREASE_QUANTITY'
+export const INCREASE_QUANTITY = 'INCREASE_QUANTITY'
+
 interface SetCartAction {
-  type: typeof SET_CART
+  readonly type: typeof SET_CART
 }
 
 interface AddToCartIdAction {
-  type: typeof ADD_ID_TO_CART
+  readonly type: typeof ADD_ID_TO_CART
   payload: {id: string, quantity: number}
 }
 
 interface RemoveFromCartIdAction {
-  type: typeof REMOVE_ID_FROM_CART
+  readonly type: typeof REMOVE_ID_FROM_CART
   payload: string
 }
 
-export type CartActionTypes = SetCartAction | AddToCartIdAction | RemoveFromCartIdAction
+interface DecreaseQuantityAction {
+  readonly type: typeof DECREASE_QUANTITY
+  payload: string
+}
+
+interface IncreaseQuantityAction {
+  readonly type: typeof INCREASE_QUANTITY
+  payload: string
+}
+
+export type CartActionTypes = SetCartAction | 
+  AddToCartIdAction | 
+  RemoveFromCartIdAction | 
+  DecreaseQuantityAction |
+  IncreaseQuantityAction
