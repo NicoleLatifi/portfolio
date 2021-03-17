ab47c64b3b60437af908e6c0ac21104a786200ea
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-16 20:35:45 -0600

    Update type names for consistency

--- src/store/products/actions.ts
+++ src/store/products/actions.ts
@@ -1,6 +1,6 @@
-import { ProductsLibrary, SET_PRODUCTS_DATA, ProductActionTypes } from './types'
+import { ProductsLibraryType, SET_PRODUCTS_DATA, ProductActionTypes } from './types'
 
-export function setProductsData(products: ProductsLibrary): ProductActionTypes {
+export function setProductsData(products: ProductsLibraryType): ProductActionTypes {
   return {
     type: SET_PRODUCTS_DATA,
     payload: products
