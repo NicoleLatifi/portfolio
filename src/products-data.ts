501175d3bac3fc5b3a565f5aaa15aee2029aee23
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-03-30 15:46:37 -0600

    Add price property

--- src/products-data.ts
+++ src/products-data.ts
@@ -5,6 +5,7 @@ export interface ProductsLibrary {
 export interface ProductItem {
   id: string,
   name: string,
+  price: number,
   rating: number,
   numberOfReviews: number
 }
@@ -13,30 +14,35 @@ export const products: ProductsLibrary = {
   "1": {
     id: "1",
     name: "Resume",
+    price: 3,
     rating: 5,
     numberOfReviews: 1
   },
   "2": {
     id: "2",
     name: "Placeholder One",
+    price: 4,
     rating: 3,
     numberOfReviews: 1
   },
   "3": {
     id: "3",
     name: "Placeholder Two",
+    price: 0,
     rating: 4,
     numberOfReviews: 1
   },
   "4": {
     id: "4",
     name: "Placeholder Three",
+    price: 0,
     rating: 0,
     numberOfReviews: 0
   },
   "5": {
     id: "5",
     name: "Placeholder Four",
+    price: 0,
     rating: 0,
     numberOfReviews: 0
   }
